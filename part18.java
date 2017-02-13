71297d38434b9d0117ec6c4d03681289fd7a0d34
Takumi Seya <b1014246@fun.ac.jp>
2017-02-14 03:53:55 +0900

    change part18 -start learning of swing-

---
+++ part18.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+
+/**
+ * @author b1014246
+ *
+ */
+public class part18 {
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+
+    javax.swing.JFrame jf = new javax.swing.JFrame();
+    jf.setBounds(100, 100, 300, 200);
+    jf.setVisible(true);
+    jf.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+  }
+}

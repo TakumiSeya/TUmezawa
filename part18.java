8d799988ec77aec5c1a747cbb0304d16dc462c8b
Takumi Seya <b1014246@fun.ac.jp>
2017-02-14 04:10:13 +0900

    change part19 -start learning of JLabel-

--- part18.java
+++ part18.java
@@ -11,8 +11,13 @@ public class part18 {
     System.out.println("Hello world");
 
     javax.swing.JFrame jf = new javax.swing.JFrame();
-    jf.setBounds(100, 100, 300, 200);
+    jf.setBounds(100, 100, 750, 400);
     jf.setVisible(true);
     jf.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+
+    javax.swing.JLabel jl = new javax.swing.JLabel("Hello world");
+    jl.setFont(new java.awt.Font(null, 0, 50));
+    jl.setForeground(java.awt.Color.RED);
+    jf.add(jl);
   }
 }
